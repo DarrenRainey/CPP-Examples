39e14e772d9377fab94cfe4476eca0531072c2c1
Darren Rainey <darren@darrenraineys.co.uk>
2017-02-16 18:01:57 +0000

    Create hello-world.cpp

---
+++ hello-world.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h> // Include standard c header file
+#include <iostream> // Include iostream for c plus plus functions
+
+int main() // Start main function
+{
+  std::cout << "Hello World" << std::endl // std::cout says print the string Hello World to terminal (Display) std::endl says add newline
+  return 0; // Exit if no errors
+}
