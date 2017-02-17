b5fafdad07a2d19cf84003a05b41a23afe478507
Darren Rainey <darren@darrenraineys.co.uk>
2017-02-17 18:30:55 +0000

    Create create-a-file.cpp

---
+++ create-a-file.cpp
@@ -0,0 +1,16 @@
+// C++ Example Code By Darren Rainey 2017 https://github.com/DarrenRainey/CPP-Examples
+
+include <stdio.h> // Import standard libarys header file
+#include <iostream> // Import IO stream header file
+#include <fstream> // Import fstream Header file for file creation
+
+using namespace std; // Tell the compiler that the following code is used in the std context
+int main() // Start Main Function
+{
+  ofstream output; // Set output as variable name
+  output.open("Hello.txt") // Create and open a file called Hello.txt
+  output << "Hello I Am A File" << std::endl; // Print Hello I Am A File to output variable
+  output.close; // Close file after writing
+
+  return 0; // Exit if no errors
+}
