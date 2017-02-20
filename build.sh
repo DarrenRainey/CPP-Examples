5d18803532ece327f62a5d460dcd3c4eddc85e35
Darren Rainey <darren@darrenraineys.co.uk>
2017-02-20 19:09:46 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ echo Building Hello World example
 gcc hello-world.cpp -o build/hello-world
 echo Building File Creation example
 gcc create-a-file.cpp -o build/create-a-file
+echo Building Alternative File Creation example
+gcc create-a-file-alternative.cpp -o build/create-a-file-alternative
 echo ""
 echo Build Complete all builds saved to $PWD/build
